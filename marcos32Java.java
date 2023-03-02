a86f1de803a22c1396fed802ef239da5324224e8
marcos <marcos@email.marcos>
2023-03-02 09:26:43 +0000

    v1

---
+++ marcos32Java.java
@@ -0,0 +1,6 @@
+public class marcos32Java.java{
+	public static void main (String[] args){
+	system.out.print("HOLA ME LLAMO MARCOS Y HE CREADO EL SERVIDOR marcos32 EN LA POBLACION MADRID");
+	}
+
+}
